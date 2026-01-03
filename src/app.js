38a647944f3904a0e04b483588a14d54274fdf10
rohitSorte02 <rohit.sorte378@gmail.com>
2026-01-03 19:23:20 +0530

    [add] :: jwt middleware to verify user to access the protected routed

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 import express from "express";
 import authRoutes from "./modules/auth/auth.routes.js";
+import { authenticate } from "./middlewares/auth.middleware.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+
+app.get("/api/userProfile", authenticate, (req, res) => {
+  res.json({
+    success:true,
+    messsage:"Access Granted",
+    userId:req.user.id,
+  });
+});
 
 app.get("/check", (req, res) => {
   res.status(200).json({status: "okkkk"});
